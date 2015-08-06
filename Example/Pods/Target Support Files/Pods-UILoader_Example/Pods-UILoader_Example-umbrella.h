5928a0591a0cae65299385dd915b93b4181e595c
Bell App Lab <apps@bellapplab.com>
2015-08-06 16:43:34 +0100

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-UILoader_Example/Pods-UILoader_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_UILoader_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UILoader_ExampleVersionString[];
+
---
+++ Pod/Classes/UILoader.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+
+//MARK: Loading Protocols
+
+@objc public protocol UILoader: NSObjectProtocol
+{
+    var loading: Bool { get set }
+    func didChangeLoadingStatus(loading: Bool)
+}
+
+
+//MARK: Extensions
+
+extension UIViewController: UILoader
+{
+    private var loadingCount: Int {
+        get {
+            if var result = objc_getAssociatedObject(self, "loadingCount") as? Int {
+                return result
+            }
+            return 0
+        }
+        set {
+            objc_setAssociatedObject(self, "loadingCount", newValue, UInt(OBJC_ASSOCIATION_RETAIN) as objc_AssociationPolicy)
+        }
+    }
+    
+    public var loading: Bool {
+        get {
+            return loadingCount > 0
+        }
+        set {
+            let oldValue = self.loading
+            
+            if newValue {
+                loadingCount += 1
+            } else {
+                loadingCount -= 1
+                if loadingCount < 0 {
+                    loadingCount = 0
+                }
+            }
+            
+            if (!oldValue && !newValue) || (newValue != oldValue)
+            {
+                self.willChangeValueForKey("loading")
+                self.didChangeLoadingStatus(newValue)
+                self.didChangeValueForKey("loading")
+            }
+        }
+    }
+    
+    public func didChangeLoadingStatus(loading: Bool)
+    {
+        
+    }
+}
