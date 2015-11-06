bb18faabc2dcce0ba5618f2979079a467aa3f74f
Bell App Lab <apps@bellapplab.com>
2015-11-06 14:16:14 +0000

    Removing the need to make UIKit compatible subclasses

--- Pod/Classes/UILoader.swift
+++ Pod/Classes/UILoader.swift
@@ -1,47 +1,84 @@
 import UIKit
 
 
-public protocol UILoader
+internal class UILoaderItem
 {
-    var _loadingCount: Int { get set }
+    weak var item: UILoader?
+    var count: Int = 0
+    init(item: UILoader) {
+        self.item = item
+    }
+}
+
+internal var items = [UILoaderItem]()
+
+internal func find(item: UILoader) -> UILoaderItem?
+{
+    for existing in items {
+        if let tempItem = existing.item {
+            if tempItem.isEqual(item) {
+                return existing
+            }
+        }
+    }
+    return nil
+}
+
+internal func make(item: UILoader) -> UILoaderItem
+{
+    if let found = find(item) {
+        return found
+    }
+    items.append(UILoaderItem(item: item))
+    return items.last!
+}
+
+
+public protocol UILoader: NSObjectProtocol
+{
     var loading: Bool { get set }
     weak var spinningThing: UIActivityIndicatorView? { get }
     func didChangeLoadingStatus(loading: Bool)
 }
 
+
 public extension UILoader
 {
     public var loading: Bool {
         get {
-            return _loadingCount > 0
+            return make(self).count > 0
         }
         set {
             let oldValue = self.loading
+            let loaderItem = make(self)
             
             var shouldNotify = false
             
             if newValue {
-                shouldNotify = ++_loadingCount == 1
+                shouldNotify = ++loaderItem.count == 1
             } else {
-                shouldNotify = --_loadingCount == 0
-                if _loadingCount < 0 {
-                    _loadingCount = 0
+                shouldNotify = --loaderItem.count == 0
+                if loaderItem.count < 0 {
+                    loaderItem.count = 0
                 }
             }
             
             if (newValue != oldValue && shouldNotify)
             {
-                let status = self.didChangeLoadingStatus
-                let thing = self.spinningThing
+                let status = self.notifyStatusChange
                 dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                    if newValue {
-                        thing?.startAnimating()
-                    } else {
-                        thing?.stopAnimating()
-                    }
                     status(newValue)
                 }
             }
         }
+    }
+    
+    private func notifyStatusChange(newValue: Bool) {
+        if newValue {
+            self.spinningThing?.startAnimating()
+        } else {
+            self.spinningThing?.stopAnimating()
+        }
+        self.didChangeLoadingStatus(newValue)
     }
 }
